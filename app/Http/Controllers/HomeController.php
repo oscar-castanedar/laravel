cc9890fbc0b1e0710685bf7373936848eac8dbe3
Oscar Castañeda <62982702+oscar-castanedar@users.noreply.github.com>
2021-02-18 14:02:41 -0600

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(){
+        $books = Book::all();
+        return view('books.index',compact('books'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+}
